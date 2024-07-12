5628f50b130a8e11a8effd30cae4e06b42eebdb5
Jeph Mari Daligdig <daligdigjeph09@gmail.com>
2024-07-12 22:38:13 +0800

    adding homepage and setting up

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Container, Col, Row } from 'reactstrap';
+
+const HomePage = () => {
+    return (
+        <Container className='homePage'>
+            
+        </Container>
+    )
+}
+
+export default HomePage;
