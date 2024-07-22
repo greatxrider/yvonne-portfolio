06c5abe717f14995bae8bc47250f7dd302115642
Jeph Daligdig <daligdig.jephmari@gmail.com>
2024-07-23 00:09:41 +0800

    added main header

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -1,11 +1,11 @@
 import './Main.module.css';
-import { Container, Col, Row } from 'reactstrap';
+import { Container, Col, Row, Button } from 'reactstrap';
 
 const Main = () => {
     return (
         <Container>
             <Row>
-                <Col>
+                <Col sm='12' md='6'>
                     <div className="oval-container">
                         <img
                             src="https://via.placeholder.com/150" // Replace with your image URL
@@ -13,6 +13,22 @@ const Main = () => {
                             className="oval-image"
                         />
                     </div>
+                </Col>
+                <Col sm='12' md='6'>
+                    <div>
+                        <h6>Hello I'M A CERTIFIED PUBLIC ACCOUNTANT</h6>
+                        <h1>Yvonne Rico</h1>
+                        <p>With over 5 years of experience in Accounting services. I help businesses and individuals,
+                            track and manage their finances and compliance with government regulations.
+                        </p>
+                    </div>
+                    <div>
+                        <Button>See my Portfolio</Button>
+                        <Button>Download Resume</Button>
+                    </div>
+                </Col>
+                <Col>
+                    
                 </Col>
             </Row>
         </Container>
